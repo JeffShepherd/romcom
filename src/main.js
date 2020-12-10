c83ec39d9b935c8ed3744e1db1a4e3e3c8069cb6
Jeff S <jeff.shep777@gmail.com>
2020-12-09 19:50:26 -0700

    Initial commit - add random bookrender on load

--- src/main.js
+++ src/main.js
@@ -1,19 +1,38 @@
-// Create variables targetting the relevant DOM elements here 👇
+//// Create variables targetting the relevant DOM elements here 👇
+
+var image = document.querySelector('.cover-image');
+var title = document.querySelector('.cover-title');
+var taglineOne = document.querySelector('.tagline-1');
+var taglineTwo = document.querySelector('.tagline-2');
 
 
-// We've provided a few variables below
+
+//// We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
 var currentCover;
 
-// Add your event listeners here 👇
+//// Add your event listeners here 👇
+window.addEventListener('load', randomBook); //with event listeners, invoking functions do not need the parens
 
 
-// Create your event handlers and other functions here 👇
 
 
-// We've provided one function to get you started
+//// Create your event handlers and other functions here 👇
+
+//assembles random book: image, title, and descriptors for tagline
+function randomBook() {
+  image.src = covers[getRandomIndex(covers)];
+  title.innerText = titles[getRandomIndex(titles)];
+  taglineOne.innerText = descriptors[getRandomIndex(descriptors)];
+  taglineTwo.innerText = descriptors[getRandomIndex(descriptors)];
+}
+
+
+
+
+//// We've provided one function to get you started
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
