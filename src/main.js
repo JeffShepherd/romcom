7d05873c756f60b26e4d3879ba1288910bcb8891
Cameron Mackintosh <cbmackintosh@outlook.com>
2020-12-10 16:53:05 -0700

    added functionality to view saved covers button

--- src/main.js
+++ src/main.js
@@ -1,15 +1,22 @@
 //// Create variables targetting the relevant DOM elements here 👇
 
+// book elements
 var image = document.querySelector('.cover-image');
 var title = document.querySelector('.cover-title');
 var taglineOne = document.querySelector('.tagline-1');
 var taglineTwo = document.querySelector('.tagline-2');
+
+// buttons
 var randomCoverButton = document.querySelector('.random-cover-button');
 var ownCoverButton = document.querySelector('.make-new-button');
 var saveCoverButton = document.querySelector('.save-cover-button');
 var homeButton = document.querySelector('.home-button');
+var viewSavedCoversButton = document.querySelector('.view-saved-button');
+
+// views
 var form = document.querySelector('.form-view');
 var completeCoverView = document.querySelector('.home-view');
+var savedCoversLibrary = document.querySelector('.saved-view')
 
 //// We've provided a few variables below
 var savedCovers = [
@@ -25,6 +32,7 @@ randomCoverButton.addEventListener('click', randomBook);
 
 ownCoverButton.addEventListener('click', displayFormView);
 
+viewSavedCoversButton.addEventListener('click', displaySavedCovers)
 
 //// Create your event handlers and other functions here 👇
 
@@ -36,7 +44,13 @@ function displayFormView() {
   randomCoverButton.classList.add('hidden'); //hide random cover button
 }
 
-
+function displaySavedCovers() {
+  savedCoversLibrary.classList.remove('hidden') // show saved cover section
+  randomCoverButton.classList.add('hidden')//hide show new random cover button
+  saveCoverButton.classList.add('hidden');//hide save cover button
+  homeButton.classList.remove('hidden');//show home button
+  completeCoverView.classList.add('hidden');//hide randomcover
+}
 
 
 //assembles random book: image, title, and descriptors for tagline
