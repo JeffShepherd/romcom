ce85683d5497a69d5401af6fc178b2d4df30158f
Jeff S <jeff.shep777@gmail.com>
2020-12-10 15:50:57 -0700

    Create new variables for required query selectors

--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ var image = document.querySelector('.cover-image');
 var title = document.querySelector('.cover-title');
 var taglineOne = document.querySelector('.tagline-1');
 var taglineTwo = document.querySelector('.tagline-2');
-
-var randomCoverButton = document.querySelector('.random-cover-button')
+var randomCoverButton = document.querySelector('.random-cover-button');
+var ownCoverButton = document.querySelector('.make-new-button');
+var saveCoverButton = document.querySelector('.save-cover-button');
+var homeButton = document.querySelector('.home-button');
+var form = document.querySelector('.form-view');
 
 //// We've provided a few variables below
 var savedCovers = [
@@ -17,11 +20,28 @@ var currentCover;
 //// Add your event listeners here 👇
 window.addEventListener('load', randomBook); //with event listeners, invoking functions do not need the parens
 
-randomCoverButton.addEventListener('click', randomBook)
+randomCoverButton.addEventListener('click', randomBook);
+
+ownCoverButton.addEventListener('click', displayFormView);
+//on click of make own cover buttom:
+
+
+
 
 //// Create your event handlers and other functions here 👇
 
 //assembles random book: image, title, and descriptors for tagline
+function displayFormView() {
+  //hide randomcover
+  //save cover button
+  //make home button appear
+  //show form
+}
+
+
+
+
+
 function randomBook() {
   currentCover = new Cover(covers[getRandomIndex(covers)], titles[getRandomIndex(titles)], descriptors[getRandomIndex(descriptors)], descriptors[getRandomIndex(descriptors)])
   image.src = currentCover.cover;
